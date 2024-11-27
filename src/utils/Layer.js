da7ee143c0f586850205b025cff827c03c320d8b
ilvme <ikangjia.cn@outlook.com>
2024-11-27 15:09:32 +0800

    feat: Toast 方法封装

---
+++ src/utils/Layer.js
@@ -0,0 +1,20 @@
+export const Toast = {
+  comingSoon() {
+    window.$message.warning('功能即将上线')
+  },
+  cancel() {
+    window.$message.warning('您已取消操作')
+  },
+  success(message) {
+    window.$message.warning(message)
+  },
+  info(message) {
+    window.$message.warning(message)
+  },
+  warning(message) {
+    window.$message.warning(message)
+  },
+  error(message) {
+    window.$message.warning(message)
+  },
+}
