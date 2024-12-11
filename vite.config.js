ec7203544f0b2299ec43a84703113499370a0749
ilvme <ikangjia.cn@outlook.com>
2024-12-11 10:15:59 +0800

    chore: 配置自动使用 Chrome 浏览器打开

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    port: 9876,
+    open: {
+      app: { name: 'chrome' },
+    },
+  },
 })
