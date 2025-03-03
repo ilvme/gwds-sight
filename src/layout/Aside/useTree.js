50b7519beefdd31e64c877e634222a9e5a444d45
kangjia <ikangjia.cn@outlook.com>
2025-03-03 13:51:47 +0800

    feat: 泛化树节点操作 hook，优化逻辑

--- src/layout/Aside/useTree.js
+++ src/layout/Aside/useTree.js
@@ -1,14 +1,27 @@
 import { reactive } from 'vue'
 
+// 树节点操作类型
+export const OP_TYPE_LIST = {
+  REMOVE: 'REMOVE', // 移除树节点
+  ADD: 'ADD', // 添加树节点
+}
+
 export const treeStore = reactive({
   flag: false,
-  node: null,
-  opType: '', // remove, add
 
-  // 刷新树, type: remove, add
-  refresh(newNode, type) {
-    this.node = newNode
-    this.opType = type
+  opType: null, // 操作类型remove, add
+
+  parentKey: null, // 当前操作节点的父节点 key
+  targetNode: null, // 待操作树节点，新增类型则为新增节点信息；删除类型则为待删除节点信息
+
+  // 刷新树总控
+  refresh(opType, targetNode, parentKey = null) {
+    if (!Object.values(OP_TYPE_LIST).includes(opType)) {
+      throw new Error('opType is not valid')
+    }
+    this.opType = opType
+    this.targetNode = targetNode
+    this.parentKey = parentKey
     this.flag = !this.flag
   },
 })
