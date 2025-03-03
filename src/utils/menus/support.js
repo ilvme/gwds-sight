22b51b10fa1dc33f3df9eafdfa6e0b021c52f74f
kangjia <ikangjia.cn@outlook.com>
2025-03-03 13:52:04 +0800

    feat: 封装树节点操作工具方法

---
+++ src/utils/menus/support.js
@@ -0,0 +1,22 @@
+const TreeUtil = {
+  /**
+   * 从 key 中解析出 datasourceId
+   * @param key
+   * @returns {*}
+   */
+  parseDatasourceId: (key) => {
+    const arr = key.split('-')
+    return arr[arr.length - 1]
+  },
+  /**
+   * 从 key 中解析并生成数据源的 key
+   * @param key
+   * @returns {*}
+   */
+  buildDatasourceKey: (key) => {
+    const arr = key.split('-')
+    return 'ds-' + arr[arr.length - 1]
+  },
+}
+
+export default TreeUtil
